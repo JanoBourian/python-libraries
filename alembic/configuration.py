3e7262b92c629809767490c01597f2dc22abd059
JanoBourian <supermath33@hotmail.com>
2023-11-29 23:26:41 -0600

    Add: first remote connection

---
+++ alembic/configuration.py
@@ -0,0 +1,16 @@
+from environs import Env
+import os
+
+def env_reader() -> Env:
+    env = Env()
+    local_path = ".env"
+    cloud_path = "config/.env.cloud"
+
+    if os.path.isfile(local_path):
+        env.read_env(local_path)
+    else:
+        env.read_env(cloud_path)
+
+    return env
+
+env = env_reader()
--- alembic/database_connection.py
+++ alembic/database_connection.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine, URL, text
 from sqlalchemy.orm import sessionmaker
+from configuration import env
 
 # connection string format: driver+postgresql://user:pass@host:port/dbname
 DATABASE_URL = URL.create(
-    drivername="postgresql+psycopg2",
-    username="postgres",
-    password="password",
-    host="localhost",
-    port=5433,
-    database="postgres",
+    drivername=env("DRIVERNAME"),
+    username=env("DBUSERNAME"),
+    password=env("PASSWORD"),
+    host=env("HOST"),
+    port=env("PORT"),
+    database=env("DATABASE"),
 )
 engine = create_engine(DATABASE_URL, echo=True)
 
@@ -17,60 +18,66 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 
 session_pool = sessionmaker(engine)
+# with session_pool() as session:
+#     session.execute(
+#         text(
+#             """
+#                          CREATE TABLE users(
+#                              telegram_id BIGINT PRIMARY KEY,
+#                              fullname VARCHAR(255) NOT NULL,
+#                              username VARCHAR(255) NOT NULL,
+#                              language_code VARCHAR(255) NOT NULL,
+#                              created_at TIMESTAMP DEFAULT NOW(),
+#                              referred_id BIGINT,
+#                              FOREIGN KEY (referred_id) 
+#                              REFERENCES users (telegram_id)
+#                              ON DELETE SET NULL
+#                          )
+#                          """
+#         )
+#     )
+#     session.execute(
+#         text(
+#             """
+#                          INSERT INTO users (
+#                              telegram_id,
+#                              fullname,
+#                              username,
+#                              language_code
+#                              ) 
+#                          VALUES (
+#                              1,
+#                              'Minerva',
+#                              'minerva37',
+#                              'en'
+#                          )
+#                          """
+#         )
+#     )
+#     session.execute(
+#         text(
+#             """
+#                          INSERT INTO users (
+#                              telegram_id,
+#                              fullname,
+#                              username,
+#                              language_code,
+#                              referred_id
+#                              ) 
+#                          VALUES (
+#                              2,
+#                              'Jano Bourian',
+#                              'janobourian',
+#                              'en',
+#                              1
+#                          )
+#                          """
+#         )
+#     )
+#     session.commit()
+
 with session_pool() as session:
-    session.execute(
-        text(
-            """
-                         CREATE TABLE users(
-                             telegram_id BIGINT PRIMARY KEY,
-                             fullname VARCHAR(255) NOT NULL,
-                             username VARCHAR(255) NOT NULL,
-                             language_code VARCHAR(255) NOT NULL,
-                             created_at TIMESTAMP DEFAULT NOW(),
-                             referred_id BIGINT,
-                             FOREIGN KEY (referred_id) 
-                             REFERENCES users (telegram_id)
-                             ON DELETE SET NULL
-                         )
-                         """
-        )
-    )
-    session.execute(
-        text(
-            """
-                         INSERT INTO users (
-                             telegram_id,
-                             fullname,
-                             username,
-                             language_code
-                             ) 
-                         VALUES (
-                             1,
-                             'Minerva',
-                             'minerva37',
-                             'en'
-                         )
-                         """
-        )
-    )
-    session.execute(
-        text(
-            """
-                         INSERT INTO users (
-                             telegram_id,
-                             fullname,
-                             username,
-                             language_code,
-                             referred_id
-                             ) 
-                         VALUES (
-                             2,
-                             'Jano Bourian',
-                             'janobourian',
-                             'en',
-                             1
-                         )
-                         """
-        )
-    )
-    session.commit()
+    results = session.execute(text("""
+                                  SELECT * FROM users;"""))
+    for row in results:
+        print(row)
